eb144f0494a123dfd58fcd3e63e9295e44b0e723
Stefan <stefan38164@gmail.com>
2023-05-01 18:14:14 +0200

    Izmenjeno prema zahtevima reviwera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 
@@ -10,7 +10,8 @@ import { CalculatorComponent } from './calculator/calculator.component';
     CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+enum CalculatorFunction {
+  Clear = 'c',
+  Percent = '%',
+  Divide = '/',
+  Multiply = '*',
+  Subtract = '-',
+  Add = '+',
+  Equals = '=',
+};
+enum ButtonType {
+ Number = 'number',
+ Function = 'function'
+};
+
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
@@ -7,21 +21,62 @@ import { Component } from '@angular/core';
 })
 export class CalculatorComponent {
   //pocetne vrednosti u templejtu
-
-  calValue: number = 0;
-  funcT: any = 'NoFunction';
-
+  private _inputValue = 0;
+  calValue = 0;
+  funcT='No Function';
+  get inputValue() {
+    return this._inputValue;
+  }
+  
+  set inputValue(value) {
+    this._inputValue = +value;
+    this.calValue = this._inputValue;
+  }
   // pocetne vrednosti u logici
-  calNumber: string = 'noValue';
 
-  firstNumber: number = 0;
-  secondNumber: number = 0;
+  calNumber = 'noValue';
+  firstNumber = 0;
+  secondNumber = 0;
+  
 
+  
+  buttonRows = [
+    [
+      { label: 'C', value:  CalculatorFunction.Clear, type: ButtonType.Function, width: 50 },
+      { label: '%', value:  CalculatorFunction.Percent, type: ButtonType.Function, width: 25 },
+      { label: '/', value:  CalculatorFunction.Divide, type: ButtonType.Function, width: 25 },
+    ],
+    [
+      { label: '7', value: '7', type: ButtonType.Number, width: 25 },
+      { label: '8', value: '8', type: ButtonType.Number, width: 25 },
+      { label: '9', value: '9', type: ButtonType.Number, width: 25 },
+      { label: '*', value:  CalculatorFunction.Multiply,  type: ButtonType.Function, width: 25 },
+    ],
+    [
+      { label: '4', value: '4', type: ButtonType.Number, width: 25 },
+      { label: '5', value: '5', type: ButtonType.Number, width: 25 },
+      { label: '6', value: '6', type: ButtonType.Number, width: 25 },
+      { label: '-', value:  CalculatorFunction.Subtract, type: ButtonType.Function, width: 25 },
+    ],
+    [
+      { label: '1', value: '1', type: ButtonType.Number, width: 25 },
+      { label: '2', value: '2', type: ButtonType.Number, width: 25 },
+      { label: '3', value: '3', type: ButtonType.Number, width: 25 },
+      { label: '+', value:  CalculatorFunction.Add, type: ButtonType.Function, width: 25 },
+    ],
+    [
+      { label: '0', value: '0', type: ButtonType.Number, width: 50 },
+      { label: '.', value: '.', type: ButtonType.Number, width: 25 },
+      { label: '=', value: CalculatorFunction.Equals, type: ButtonType.Function, width: 25 },
+    ],
+  ];
+  
+  
   //kada se klikne na dugme
   onClickValue(val: string, type: any) {
-    if (type === 'number') {
+    if (type === ButtonType.Number) {
       this.onNumberClick(val);
-    } else if (type === 'function') {
+    } else if (type === ButtonType.Function) {
       this.onFunctionClick(val);
     }
   }
@@ -33,15 +88,17 @@ export class CalculatorComponent {
     } else {
       this.calNumber = val;
     }
-    this.calValue = parseFloat(this.calNumber);
+    this.calValue = +this.calNumber;
+    this.inputValue=this.calValue;
   }
 
   //ako se kliknulo na operator
   onFunctionClick(val: string) {
-    if (val === 'c') {
+    if (val ===  CalculatorFunction.Clear) {
       this.clearAll();
     } else if (this.funcT === 'NoFunction') {
       this.firstNumber = this.calValue;
+      this.inputValue = this.firstNumber;
       this.calValue = this.firstNumber;
       this.calNumber = 'noValue';
       this.funcT = val;
@@ -52,36 +109,40 @@ export class CalculatorComponent {
   }
 
   //izracunavanje
-  valueCalculate(val: string) {
-    if (this.funcT === '+') {
-      const Total = this.firstNumber + this.secondNumber;
-      this.totalAssignValues(Total, val);
+  valueCalculate(val:string) {
+    let Total: number;
+
+    switch (this.funcT) {
+      case  CalculatorFunction.Add:
+        Total = this.firstNumber + this.secondNumber;
+        break;
+      case  CalculatorFunction.Subtract:
+        Total = this.firstNumber - this.secondNumber;
+        break;
+      case  CalculatorFunction.Multiply:
+        Total = this.firstNumber * this.secondNumber;
+        break;
+      case  CalculatorFunction.Divide:
+        Total = this.firstNumber / this.secondNumber;
+        break;
+      case  CalculatorFunction.Percent:
+        Total = (this.firstNumber * this.secondNumber) / 100;
+        break;
+      default:
+        throw new Error(`Invalid operator: ${this.funcT}`);
     }
-    if (this.funcT === '-') {
-      const Total = this.firstNumber - this.secondNumber;
-      this.totalAssignValues(Total, val);
-    }
-    if (this.funcT === '*') {
-      const Total = this.firstNumber * this.secondNumber;
-      this.totalAssignValues(Total, val);
-    }
-    if (this.funcT === '/') {
-      const Total = this.firstNumber / this.secondNumber;
-      this.totalAssignValues(Total, val);
-    }
-    if (this.funcT === '%') {
-      const Total = this.firstNumber * this.secondNumber /100;
-      this.totalAssignValues(Total, val);
-    }
+
+    this.totalAssignValues(Total, val);
   }
 
   totalAssignValues(Total: number, val: string) {
     this.calValue = Total;
     this.firstNumber = Total;
+    this.inputValue = Total;
     this.secondNumber = 0;
     this.calNumber = 'noValue';
     this.funcT = val;
-    if (val === '=') {
+    if (val ===  CalculatorFunction.Equals) {
       this.onEqualPress();
     }
   }
@@ -101,6 +162,6 @@ export class CalculatorComponent {
     this.calValue = 0;
     this.funcT = 'NoFunction';
     this.calNumber = 'noValue';
+    this.inputValue = 0;
   }
 }
-
